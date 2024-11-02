b72bdcd91fc9bdbc400ab2c8e383518b74e52e2c
Simon <sstur@me.com>
2024-11-02 10:50:41 -0700

    Get hello world working in Tauri

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import { useState } from 'react';
+import { invoke } from '@tauri-apps/api/core';
+import './App.css';
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+export function App() {
+  const [greetMsg, setGreetMsg] = useState('');
+  const [name, setName] = useState('');
 
-  async function greet() {
+  const greet = async () => {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+    const result = await invoke('greet', { name });
+    setGreetMsg(String(result));
+  };
 
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+    <main>
+      <h1>Hello world</h1>
 
       <form
-        className="row"
+        style={{ display: 'flex', gap: '1rem' }}
         onSubmit={(e) => {
           e.preventDefault();
           greet();
@@ -47,5 +34,3 @@ function App() {
     </main>
   );
 }
-
-export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const root = document.createElement('div');
+root.id = 'root';
+document.body.appendChild(root);
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
